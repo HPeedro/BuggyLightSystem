a216bdd4899ad633da25955b8fb930210a98f95c
Pedro <peedrobr@gmail.com>
2020-11-10 21:33:33 -0300

    Cop mode
    added cop mode and properly commented /formatted params and constants

--- ArduinoCode/BuggyLights/BuggyLights.ino
+++ ArduinoCode/BuggyLights/BuggyLights.ino
@@ -1,3 +1,5 @@
+//IMPORTS
+//#include <BuggyLights_Functions.ino>
 #include <FastLED.h>
 
 //PIN_OUTPUT
@@ -17,34 +19,51 @@
 #define INPUT_REVERSE_LIGHT A0
 
 //BLINKERS
-#define BLINK_DELAY 450
+#define BLINK_DELAY 450 // - blinker interval delay
 #define BLINK_TIMER_NAME "BLINK"
 #define BLINK_TIMER_SHUT "DONT"
 #define BLINK_NONE 0
 #define BLINK_LEFT 1
 #define BLINK_RIGHT 2
 #define BLINK_BOTH 3
+
 //CLK AND TIMER
-#define TIMER_LIST_SIZE 5
+#define TIMER_LIST_SIZE 5 // - number of individual timer slots
 String timerNames[TIMER_LIST_SIZE];
 long timerMillis[TIMER_LIST_SIZE];
-//IMPORTS
-//#include <BuggyLights_Functions.ino>
-//COLLORS
-CRGB c_blink = CRGB(255,255,0);
-CRGB c_break = CRGB(255,0,0);
-CRGB c_break_low = CRGB(100,0,0);
-CRGB c_daylight = CRGB(255,255,255);
-CRGB c_reverse = CRGB(255,255,255);
-//ARRAY_SIZE
-#define REAR_SIZE 5
+
+//COLLORS DEFAULT
+#define LIGHT_BRIGHTNESS 10 // - general light brightness
+CRGB c_blink = CRGB(255,255,0); // - blinker color
+CRGB c_break = CRGB(255,0,0); // - break light color
+CRGB c_break_low = CRGB(100,0,0); // - rear light color
+CRGB c_daylight = CRGB(255,255,255); // - daylight color
+CRGB c_reverse = CRGB(255,255,255); // - reverse light color
+
+//ARRAY_RGB_DEFAULT
+#define REAR_SIZE 5 // - number of leds on each rear light
 CRGB rear_left[REAR_SIZE];
 CRGB rear_right[REAR_SIZE];
-#define FRONT_SIZE 5
+#define FRONT_SIZE 5 // - number of leds on each front light
 CRGB front_left[FRONT_SIZE];
 CRGB front_right[FRONT_SIZE];
+
+//COLLORS ALTERNATIVE
+CRGB c_cop_red = CRGB(255,0,0); // - cop light red
+CRGB c_cop_blue = CRGB(0,0,255); // - cop light blue
+
+//ARRAY_RGB_ALTERNATIVE
+CRGB all_cop[] = {CRGB(255,0,0),CRGB(0,0,0),CRGB(255,0,0),CRGB(0,0,0),CRGB(255,0,0),CRGB(0,0,0),CRGB(255,0,0),CRGB(0,0,0),
+                  CRGB(0,0,255),CRGB(0,0,0),CRGB(0,0,255),CRGB(0,0,0),CRGB(0,0,255),CRGB(0,0,0),CRGB(0,0,255),CRGB(0,0,0)};
+                  // - cop light sequence
+byte all_cops_pos = 0;
+#define ALL_COPS_SIZE 16 // - cop light sequence size
+#define ALL_COPS_DELAY 55 // - cop light interval delay
+#define COP_DELAY "COP"
+
 //DEBUG
-#define DEBUG_ENABLED 1
+#define DEBUG_ENABLED 0 // - 1 = enable 
+#define DEV_MODE 1 // - 1 = enable
 
 //GLOBAL_VARS
 byte blinkStatus = 0;
@@ -54,15 +73,7 @@ byte blinkStatus = 0;
    3 - both
 */
 void setup() {
-  //take the traash out (arrays)
-  for(int i = 0; i < REAR_SIZE; i++){
-    rear_left[i] = CRGB(0,0,0);
-    rear_right[i] = CRGB(0,0,0);
-  }
-  for(int i = 0; i < FRONT_SIZE; i++){
-    front_left[i] = CRGB(0,0,0);
-    front_right[i] = CRGB(0,0,0);
-  }
+  
   //pinMode
   pinMode(INPUT_DAY_LIGHT, INPUT_PULLUP);
   pinMode(INPUT_LOW_LIGHT, INPUT_PULLUP);
@@ -72,33 +83,43 @@ void setup() {
   pinMode(INPUT_BLINK_BOTH, INPUT_PULLUP);
   pinMode(INPUT_BRAKE_LIGHT, INPUT_PULLUP);
   pinMode(INPUT_REVERSE_LIGHT, INPUT_PULLUP);
+  
   //Set led things
   FastLED.addLeds<WS2812, PIN_FRONT_LEFT, GRB>(front_left, FRONT_SIZE);
   FastLED.addLeds<WS2812, PIN_FRONT_RIGHT, GRB>(front_right, FRONT_SIZE);
   FastLED.addLeds<WS2812, PIN_REAR_LEFT, GRB>(rear_left, REAR_SIZE);
   FastLED.addLeds<WS2812, PIN_REAR_RIGHT, GRB>(rear_right, REAR_SIZE);
+
+  FastLED.setBrightness(LIGHT_BRIGHTNESS);
+  
   //DEBUG
   if (DEBUG_ENABLED == 1) {
     Serial.begin(9600);
   }
+  
+  //take the traash out
+  for(int i = 0; i < REAR_SIZE; i++){
+    rear_left[i] = CRGB(0,0,0);
+    rear_right[i] = CRGB(0,0,0);
+  }
+  for(int i = 0; i < FRONT_SIZE; i++){
+    front_left[i] = CRGB(0,0,0);
+    front_right[i] = CRGB(0,0,0);
+  }
+  FastLED.clear(true);
 }
 
 void loop() {
-  
-  byte _blink = getBlinkerInput();
-  
-  blinker(_blink);
-  FastLED.show();
-  dayLightAndBreak();
 
-  if(awaits(BLINK_TIMER_SHUT)){
-    Serial.println(blinkStatus);
-    Serial.println("awaits");
+  if(DEV_MODE == 0){
+    //NORMAL OPERATION
+    byte _blink = getBlinkerInput();
+    blinker(_blink);
+    dayLightAndBreak();
   } else {
-    Serial.println(blinkStatus);
-    Serial.println("does not");
+    //CUSTOM DEV OPERATIONS
+    copMode();
   }
-  
   FastLED.show();
 }
 
@@ -108,31 +129,29 @@ void dayLightAndBreak() {
   bool _lowLight = analogPinRead(INPUT_LOW_LIGHT);
   bool _breakLight = analogPinRead(INPUT_BRAKE_LIGHT);
   if(!(awaits(BLINK_TIMER_SHUT) && (blinkStatus == BLINK_BOTH || blinkStatus == BLINK_RIGHT))){
-    Serial.println("JOIN RIGHT");
     if (_lowLight) {
-      arrayFullChange(front_right, c_daylight);
+      arrayFullChange(front_right, FRONT_SIZE, c_daylight);
       if (_breakLight) {
-        arrayFullChange(rear_right, c_break);
+        arrayFullChange(rear_right, REAR_SIZE, c_break);
       } else {
-        arrayFullChange(rear_right, c_break_low);
+        arrayFullChange(rear_right, REAR_SIZE, c_break_low);
       }
     } else {
-      arrayFullChange(front_right, c_daylight);
-      arrayFullChange(rear_right, c_break);
+      arrayFullChange(front_right, FRONT_SIZE, c_daylight);
+      arrayFullChange(rear_right, REAR_SIZE, c_break);
     }
   }
   if(!(awaits(BLINK_TIMER_SHUT) && (blinkStatus == BLINK_BOTH || blinkStatus == BLINK_LEFT))){
-    Serial.println("JOIN LEFT");
     if (_lowLight) {
-      arrayFullChange(front_left, c_daylight);
+      arrayFullChange(front_left, FRONT_SIZE, c_daylight);
       if (_breakLight) {
-        arrayFullChange(rear_left, c_break);
+        arrayFullChange(rear_left, REAR_SIZE, c_break);
       } else {
-        arrayFullChange(rear_left, c_break_low);
+        arrayFullChange(rear_left, REAR_SIZE, c_break_low);
       }
     } else {
-      arrayFullChange(front_left, c_daylight);
-      arrayFullChange(rear_left, c_break);
+      arrayFullChange(front_left, FRONT_SIZE, c_daylight);
+      arrayFullChange(rear_left, REAR_SIZE, c_break);
     }
   }
   //analogPinRead(INPUT_BRAKE_LIGHT);
@@ -179,18 +198,18 @@ bool blinker(byte _mode) {
     timer(BLINK_DELAY, BLINK_TIMER_SHUT);
     switch (_mode) {
       case BLINK_LEFT:
-        arrayFullChange(front_left, c_blink);
-        arrayFullChange(rear_left, c_blink);
+        arrayFullChange(front_left, FRONT_SIZE, c_blink);
+        arrayFullChange(rear_left, REAR_SIZE, c_blink);
         break;
       case BLINK_RIGHT:
-        arrayFullChange(front_right, c_blink);
-        arrayFullChange(rear_right, c_blink);
+        arrayFullChange(front_right, FRONT_SIZE, c_blink);
+        arrayFullChange(rear_right, REAR_SIZE, c_blink);
         break;
       case BLINK_BOTH:
-        arrayFullChange(front_left, c_blink);
-        arrayFullChange(front_right, c_blink);
-        arrayFullChange(rear_left, c_blink);
-        arrayFullChange(rear_right, c_blink);
+        arrayFullChange(front_left, FRONT_SIZE, c_blink);
+        arrayFullChange(front_right, FRONT_SIZE, c_blink);
+        arrayFullChange(rear_left, REAR_SIZE, c_blink);
+        arrayFullChange(rear_right, REAR_SIZE, c_blink);
         break;
       default:
         break;
@@ -200,13 +219,28 @@ bool blinker(byte _mode) {
     if (_mode != BLINK_NONE) {
       timer(BLINK_DELAY, BLINK_TIMER_NAME);
     }
-    //arrayFullChange(front_left, c_daylight);
-    //arrayFullChange(front_right, c_daylight);
-    //arrayFullChange(rear_left, c_break_low);
-    //arrayFullChange(rear_right, c_break_low);
   }
 }
 //END OF BLINKER
+//COP MODE
+void copMode(){
+  if(clk(COP_DELAY)){
+    timer(ALL_COPS_DELAY,COP_DELAY);
+    all_cops_pos++;
+    if(all_cops_pos >= ALL_COPS_SIZE){
+      all_cops_pos = 0;
+    }
+    CRGB _color = all_cop[all_cops_pos];
+    arrayFullChange(front_left, FRONT_SIZE, _color);
+    arrayFullChange(front_right, FRONT_SIZE, _color);
+    arrayFullChange(rear_left, REAR_SIZE, _color);
+    arrayFullChange(rear_right, REAR_SIZE, _color);
+  }
+  if(!awaits(COP_DELAY)){
+    timer(ALL_COPS_DELAY,COP_DELAY);
+  }
+}
+//END COP MODE
 //CLK AND TIMER
 bool clk(String _clk) {
   int index = -1;
@@ -257,8 +291,8 @@ bool removeClk(String _clk) {
 }
 //END OF CLK AND TIMER
 //ARRAY FUNCTIONS
-void arrayFullChange(CRGB _array[], CRGB _color){
-  int _size = sizeof(_array);
+void arrayFullChange(CRGB _array[],int _size, CRGB _color){
+  //int _size = sizeof(_array);
   for(int i = 0; i < _size; i++){
     _array[i] = _color;
   }
